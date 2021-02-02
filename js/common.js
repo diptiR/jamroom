d72c670f124cdeea01b01bbe80dc0502f7fcac7b
Pooja <poojaredkar89@gmail.com>
2021-02-02 16:03:59 +0530

    Showreel, common js added and some updations

---
+++ js/common.js
@@ -0,0 +1,17 @@
+function searchFunction(event) {
+    event.preventDefault();
+    document.getElementById('inputSearch').className = "showSearch form-control"
+}
+
+function onYouTubeIframeAPIReady() {
+    var player;
+    player = new YT.Player('player', {
+      videoId: 'M7lc1UVf-VE',
+      playerVars: { 'autoplay': 1, 'controls': 0 },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange,
+        'onError': onPlayerError
+      }
+    });
+  }
